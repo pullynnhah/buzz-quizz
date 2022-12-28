d8c56cd9f357505970a1aeec759dd67b925c9ca2
Paula Rodrigues <pullynnhah@icloud.com>
2022-12-28 09:47:52 -0300

    questions for create quiz

--- scripts/create.js
+++ scripts/create.js
@@ -16,6 +16,7 @@ function renderBasicInfo() {
         <input
         class="quizz-image"
         type="url"
+        required
         placeholder="URL da imagem do seu quizz" />
         <p class="err">O valor informado não é uma URL válida</p>
         </div>
@@ -50,8 +51,7 @@ function getBasicInfo() {
 
   const quizzTitleCond =
     quizzTitle.value.length < 20 || quizzTitle.value.length > 65;
-  const quizzImageCond =
-    quizzImage.value.length === 0 || !quizzImage.checkValidity();
+  const quizzImageCond = !quizzImage.checkValidity();
   const quizzQuestionsAmountCond =
     isNaN(quizzQuestionsAmount.value) || Number(quizzQuestionsAmount.value) < 3;
   const quizzLevelsCond =
@@ -76,6 +76,91 @@ function getBasicInfo() {
   }
 }
 
+function renderQuestions() {
+  let html = "";
+  for (let i = 1; i <= createQuestionsCount; i++) {
+    html += `
+    <div class="container">
+    <details>
+      <summary>
+        <h3>Pergunta ${i}</h3>
+        <ion-icon name="create-outline"></ion-icon>
+      </summary>
+      <div class="form">
+        <div>
+          <input
+            class="quizz-question-text"
+            type="text"
+            placeholder="Texto da pergunta" />
+          <p class="err">O texto deve ter no mínimo 20 caracteres</p>
+        </div>
+        <div>
+          <input
+            class="quizz-question-color"
+            type="text"
+            placeholder="Cor de fundo da pergunta" />
+          <p class="err">Cor de fundo deve estar em HEX</p>
+        </div>
+
+        <h3>Resposta correta</h3>
+        ${getPair(0)}
+
+        <h3>Respostas incorretas</h3>
+        ${getPair(1)}
+        ${getPair(2)}
+        ${getPair(3)}
+      </div>
+    </details>
+    </div>
+    `;
+  }
+
+  main.innerHTML = `
+  <div class="create-quizz">
+    <h2>Crie suas perguntas</h2>
+      ${html}
+    <button class="red-btn" onclick="getQuestions()">
+      Prosseguir pra criar níveis
+    </button>
+  </div>
+  `;
+  const dets = document.querySelectorAll("details");
+  dets.forEach(target =>
+    target.addEventListener("click", () =>
+      dets.forEach(det => {
+        if (det !== target) {
+          det.removeAttribute("open");
+        }
+      })
+    )
+  );
+}
+
+function getPair(index) {
+  const cls = index === 0 ? "right" : "wrong";
+  const placeholder1 = index === 0 ? "correta" : `incorreta ${index}`;
+  const placeholder2 = index === 0 ? "" : `${index}`;
+  return `
+  <div class="pair">
+    <div>
+      <input
+        class="quizz-${cls}-answer"
+        type="text"
+        placeholder="Resposta ${placeholder1}" />
+      <p class="err">A resposta não pode ser vazia</p>
+    </div>
+    <div>
+      <input
+        class="quizz-${cls}-answer-img"
+        type="url"
+        required
+        placeholder="URL da imagem ${placeholder2}" />
+      <p class="err">O valor informado não é uma URL válida</p>
+    </div>
+  </div>
+  `;
+}
+
 function displayErr(el, cond) {
   if (cond) el.parentNode.classList.add("input-err");
   else el.parentNode.classList.remove("input-err");
@@ -83,7 +168,8 @@ function displayErr(el, cond) {
 
 let createQuestions;
 let createLevels;
-let createQuestionsCount;
+let createQuestionsCount = 3;
 let createLevelsCount;
 
-renderBasicInfo();
+// renderBasicInfo();
+renderQuestions();
--- scripts/helpers.js
+++ scripts/helpers.js
@@ -19,3 +19,10 @@ function scroll(el) {
     inline: "center",
   });
 }
+
+function verifyHex(hex) {
+  if (/^#([\dA-Fa-f]{3}){1,2}$/i.test(hex)) {
+    return hex;
+  }
+  return false;
+}
