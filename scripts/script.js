cea1a6b56baf0c2fec65f8154771827dd8747e7e
Paula Rodrigues <pullynnhah@icloud.com>
2022-12-27 21:52:48 -0300

    implement style for list of quizzes

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 const URI = "https://mock-api.driven.com.br/api/v4/buzzquizz";
+
+function playTemp() {
+  console.log("PLAY");
+}
+
+function editTemp(e) {
+  e.stopPropagation();
+  console.log("EDIT");
+}
+
+function delTemp(e) {
+  e.stopPropagation();
+  console.log("DEL");
+}
+
+function createTemp() {
+  console.log("CREATE");
+}
